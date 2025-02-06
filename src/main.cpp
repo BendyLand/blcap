559e6dc7f8bd6589fe95a0fde9782ff1aed44d9b
BendyLand <128928536+BendyLand@users.noreply.github.com>
2025-02-05 20:27:10 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <fstream>
+#include <cstdarg>
+#include "os.hpp"
+#include "mystl.hpp"
+
+using std::cout;
+using std::endl;
+
+std::string get_output_name(const std::string& input_file);
+std::string format_str(const std::string text, ...);
+std::string get_transcription(const std::string& output);
+std::string read_file(const std::string& filename);
+std::string transcription_to_srt(const std::string& transcript);
+void write_file(const std::string& filename, const std::string& text);
+
+int main(int argc, char** argv)
+{
+    if (argc > 1) {
+        std::string filename = argv[1];
+        std::string output = get_output_name(filename) + ".wav";
+        std::string extraction_cmd = format_str("ffmpeg -i %s -vn -acodec pcm_s16le -ar 16000 -ac 1 %s", filename.c_str(), output.c_str());
+        std::pair<int, std::string> res = OS::run_command(extraction_cmd);
+        // The whisper command is: <path>/<to>/whisper-cpp/build/bin/whisper-cli -m <path>/<to>/whisper-cpp/models/ggml-base.en.bin -f <WAV_file>  
+        std::string whisper_cmd = read_file("whisper-cmd.txt") + output; 
+        std::pair<int, std::string> res2 = OS::run_command(whisper_cmd);
+        std::string transcription = get_transcription(res2.second);
+        transcription = transcription_to_srt(transcription);
+        write_file("transcript.srt", transcription);
+        std::string transcript_path = read_file("transcript-path.txt");
+        std::string burn_cmd = format_str("ffmpeg -i %s -vf subtitles=%s -c:a copy %s_w_captions.mp4", filename.c_str(), transcript_path.c_str(), get_output_name(filename).c_str());
+        std::pair<int, std::string> res3 = OS::run_command(burn_cmd);
+        if (res3.first == 0) cout << "Captions added successfully!" << endl;
+    }
+    else {
+        cout << "Usage: blcap <video_file>" << endl;
+    }
+    return 0;
+}
+
+std::string format_str(const std::string text, ...)
+{
+    va_list args;
+    va_start(args, text);
+    int size = vsnprintf(nullptr, 0, text.c_str(), args); // Get required size
+    va_end(args);
+    if (size < 0) return "";  
+    std::string buffer(size, '\0'); // Allocate std::string with correct size
+    va_start(args, text);
+    vsnprintf(&buffer[0], size + 1, text.c_str(), args); // Fill buffer
+    va_end(args);
+    return buffer;
+}
+
+std::string get_output_name(const std::string& input_file)
+{
+    size_t pos = input_file.find(".");
+    if (pos == -1) return input_file;
+    return input_file.substr(0, pos);
+}
+
+int transcribe_audio(const std::string& audioFile, const std::string& transcriptFile) 
+{
+    std::string command = "./whisper -m models/medium.en.bin -f " + audioFile + " -otxt";
+    return system(command.c_str());
+}
+
+std::string read_file(const std::string& filename)
+{
+    std::fstream file(filename.c_str());
+    if (!file.is_open()) {
+        std::cerr << "Error opening file." << endl;
+        exit(1);
+    }
+    char c;
+    std::string result = "";
+    while (file.get(c)) result += c;
+    return result;
+}
+
+void write_file(const std::string& filename, const std::string& text)
+{
+    std::ofstream file(filename.c_str());
+    if (file.is_open()) {
+        file << text;
+        return;
+    }
+    std::cerr << "Error opening file" << endl;
+    exit(1);
+}
+
+std::string get_transcription(const std::string& output)
+{
+    my::string temp = output;
+    my::vector<my::string> lines = temp.split("\n");
+    my::vector<my::string> temp_vec;
+    for (const my::string& line : lines) {
+        if (line.str().starts_with("[")) temp_vec.emplace(line);
+    }
+    return temp_vec.join("\n");
+}
+
+my::string convert_line(const my::string& line, size_t current)
+{
+    size_t end_bracket = line.str().find("]") + 1;
+    my::string timestamp = line.str().substr(0, end_bracket);
+    while (timestamp.contains(".")) {
+        size_t pos = timestamp.str().find(".");
+        timestamp[pos] = ',';
+    }
+    timestamp = timestamp.str().substr(1, timestamp.size()-2);
+    my::string text = line.str().substr(end_bracket+1);
+    text = text.trim();
+    my::string result = format_str("%d\n%s\n%s", current, timestamp.str().c_str(), text.str().c_str());
+    return result;
+}
+
+std::string transcription_to_srt(const std::string& transcript)
+{
+    my::string temp = transcript;
+    my::vector<my::string> lines = temp.split("\n");
+    size_t current = 1;
+    my::vector<my::string> temp_vec;
+    for (my::string line : lines) {
+        my::string new_line = convert_line(line, current++);
+        temp_vec.emplace(new_line);
+    }
+    return temp_vec.join("\n\n").trim();
+}
+
+
---
+++ src/mystl.hpp
@@ -0,0 +1,260 @@
+#pragma once
+
+#include <exception>
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+namespace my
+{
+    class string
+    {
+    private:
+        // Fields
+        std::string m_data;
+    public:
+        // Constructor, Copy/Move Constructors, Destructor
+        string() : m_data("") {}
+        string(const char* str) : m_data(str) {}
+        string(const std::string& str) : m_data(str) {}
+        string(const my::string& str) : m_data(str.m_data) {}
+        string(my::string&& str) noexcept : m_data(std::move(str.m_data)) {}
+        ~string() = default;
+
+        // Operators
+        inline operator std::string() const { return m_data; }
+        inline string& operator=(const string& other)
+        {
+            m_data = other.m_data;
+            return *this;
+        }
+        inline string& operator=(string&& other) noexcept 
+        {
+            m_data = std::move(other.m_data);
+            return *this;
+        }
+        inline string& operator+=(const string& other)
+        {
+            m_data += other.m_data;
+            return *this;
+        }
+        inline string& operator<<(const string& other)
+        {
+            m_data += other.m_data;
+            return *this;
+        }
+        inline friend std::ostream& operator<<(std::ostream& os, const string& other)
+        {
+            os << other.m_data;
+            return os;
+        }
+        inline char& operator[](int index)
+        {
+            if (index >= 0) {
+                if (index >= m_data.size()) throw std::out_of_range("Index out of bounds");
+                return m_data[index];
+            }
+            do {
+                index = m_data.size() + index;
+            } while (index < 0);
+            return m_data[index];
+        }
+        inline const char& operator[](int index) const
+        {
+            size_t size = m_data.size();
+            if (index >= 0) {
+                if (index >= size) throw std::out_of_range("Index out of bounds");
+                return m_data[index];
+            }
+            do {
+                index = size + index;
+            } while (index < 0);
+            return m_data[index];
+        }
+
+        // Methods
+        inline const std::string str() const { return m_data; }
+        inline std::string str() { return m_data; }
+        inline bool contains(const std::string& substr) const
+        {
+            return m_data.find(substr) != std::string::npos;
+        }
+        inline string ltrim() const
+        {
+            size_t start = m_data.find_first_not_of(" \t\n\r");
+            return (start == std::string::npos) ? "" : m_data.substr(start);
+        }
+        inline string rtrim() const
+        {
+            size_t end = m_data.find_last_not_of(" \t\n\r");
+            return (end == std::string::npos) ? "" : m_data.substr(0, end + 1);
+        }
+        inline string trim() const
+        {
+            size_t start = m_data.find_first_not_of(" \t\n\r");
+            size_t end = m_data.find_last_not_of(" \t\r\n");
+            return (start == std::string::npos) ? "" : m_data.substr(start, end - start + 1);
+        }
+        inline string to_upper() const
+        {
+            string result = m_data;
+            std::transform(result.m_data.begin(), result.m_data.end(), result.m_data.begin(), ::toupper);
+            return result;
+        }
+        inline string to_lower() const
+        {
+            string result = m_data;
+            std::transform(result.m_data.begin(), result.m_data.end(), result.m_data.begin(), ::tolower);
+            return result;
+        }
+        inline string to_upper(size_t from) const
+        {
+            string result = m_data;
+            std::transform(result.m_data.begin()+from, result.m_data.end(), result.m_data.begin()+from, ::toupper);
+            return result;
+        }
+        inline string to_lower(size_t from) const
+        {
+            string result = m_data;
+            std::transform(result.m_data.begin()+from, result.m_data.end(), result.m_data.begin()+from, ::tolower);
+            return result;
+        }
+        inline string to_upper(size_t from, size_t to) const
+        {
+            string result = m_data;
+            std::transform(result.m_data.begin()+from, result.m_data.begin()+to, result.m_data.begin()+from, ::toupper);
+            return result;
+        }
+        inline string to_lower(size_t from, size_t to) const
+        {
+            string result = m_data;
+            std::transform(result.m_data.begin()+from, result.m_data.begin()+to, result.m_data.begin()+from, ::tolower);
+            return result;
+        }
+        inline std::vector<string> split(string delim) const 
+        {
+            std::string copy = this->m_data;
+            std::vector<string> result;
+            my::string temp; 
+            while (copy.find(delim.str()) != std::string::npos) {
+                temp = copy.substr(0, copy.find(delim.str()));
+                result.emplace_back(temp);
+                copy.erase(copy.begin(), copy.begin()+(copy.find(delim.str())+1));
+            }
+            if (copy.size() > 0) result.emplace_back(copy);
+            return result;
+        }
+
+        // Forward std::string methods
+        inline size_t size() const { return m_data.size(); }
+        inline bool empty() const { return m_data.empty(); }
+        inline void push(const char c) { m_data.push_back(c); }
+    };
+};
+
+namespace my
+{
+    template <typename T>
+    class vector
+    {
+    private:
+        // Fields
+        std::vector<T> m_data;
+    public:
+        // Constructor, Copy/Move Constructors, Destructor
+        vector() : m_data() {}
+        vector(const vector<T>& vec) : m_data(vec.begin(), vec.end()) {}
+        vector(vector<T>&& vec) noexcept : m_data(std::move(vec.m_data)) {}
+        vector(const std::vector<T>& elements) : m_data(elements.begin(), elements.end()) {}
+        vector(const std::initializer_list<T>& elements) : m_data(elements.begin(), elements.end()) {}
+
+        // Operators
+        inline operator std::vector<T>() const { return m_data; }
+        inline vector& operator=(const vector<T>& vec)
+        {
+            if (this != &vec) {
+                vector temp(vec);
+                std::swap(this->m_data, temp.m_data);
+            }
+            return *this;
+        }
+        inline vector& operator=(vector<T>&& vec) noexcept
+        {
+            if (this != &vec) {
+                m_data = std::move(vec.m_data);
+            }
+            return *this;
+        }
+        inline T& operator[](int index)
+        {
+            if (index >= 0) {
+                if (index >= this->size()) throw std::out_of_range("Index out of bounds");
+                return m_data[index];
+            }
+            do {
+                index = this->size() + index;
+            } while (index < 0);
+            return m_data[index];
+        }
+        inline const T& operator[](int index) const
+        {
+            if (index >= 0) {
+                if (index >= this->size()) throw std::out_of_range("Index out of bounds");
+                return m_data[index];
+            }
+            do {
+                index = this->size() + index;
+            } while (index < 0);
+            return m_data[index];
+        }
+        inline friend std::ostream& operator<<(std::ostream& os, my::vector<T>& items)
+        {
+            for (T& item : items) {
+                os << item << std::endl;
+            }
+            return os;
+        }
+
+        // Methods
+        template <typename... Args>
+        inline void emplace(Args&&... args) { m_data.emplace_back(std::forward<Args>(args)...); }
+        inline void push(const T& value) { m_data.push_back(value); }
+        inline void push(T&& value) { m_data.push_back(value); }
+        inline T pop()
+        {
+            if (this->size() == 0) throw std::length_error("Vector is empty.");
+            T result = m_data[m_data.size()-1];
+            m_data.pop_back();
+            return result;
+        }
+        template <typename U = T>
+        inline std::enable_if_t
+        <
+            std::is_same_v<U, my::string> || std::is_same_v<U, std::string>, 
+            my::string
+        >
+        join(const my::string& delim) const {
+            my::string result;
+            for (size_t i = 0; i < m_data.size(); ++i) {
+                result += m_data[i];
+                if (i < m_data.size() - 1) {
+                    result += delim;
+                }
+            }
+            return result;
+        }
+        inline size_t size() const { return m_data.size(); }
+        inline size_t capacity() const { return m_data.capacity(); }
+        inline auto begin() -> decltype(m_data.begin()) { return m_data.begin(); }
+        inline auto end() -> decltype(m_data.end()) { return m_data.end(); }
+        inline auto begin() const -> decltype(m_data.begin()) { return m_data.begin(); }
+        inline auto end() const -> decltype(m_data.end()) { return m_data.end(); }
+        inline auto cbegin() const -> decltype(m_data.cbegin()) { return m_data.cbegin(); }
+        inline auto cend() const -> decltype(m_data.cend()) { return m_data.cend(); }
+        inline auto rbegin() -> decltype(m_data.rbegin()) { return m_data.rbegin(); }
+        inline auto rend() -> decltype(m_data.rend()) { return m_data.rend(); }
+        inline auto rbegin() const -> decltype(m_data.rbegin()) { return m_data.rbegin(); }
+        inline auto rend() const -> decltype(m_data.rend()) { return m_data.rend(); }
+    };
+}
